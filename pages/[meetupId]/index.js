09c1d88bfdf8a7e937eaa54342740fec49813a9f
HAMID BERDOUS <hberdous@gmail.com>
2021-12-22 11:22:56 +0100

    update revalidate

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -56,7 +56,7 @@ export async function getStaticProps(context) {
         id: meetup._id.toString(),
       },
     },
-    revalidate: 10,
+    revalidate: 1,
   };
 }
 
--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export async function getStaticProps() {
         };
       }),
     },
-    revalidate: 10,
+    revalidate: 1,
   };
 }
 
